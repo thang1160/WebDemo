051978455f52f2939ac966068a10051557ba5226
thang1160 <35557579+thang1160@users.noreply.github.com>
2021-01-25 02:22:45 +0700

    add git hook

--- WebApp/Controllers/DefaultController.cs
+++ WebApp/Controllers/DefaultController.cs
@@ -1,22 +1,20 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using WebApp.Models;
 using WebPush;
 
 namespace FPT_Science.Controllers
 {
     public class DefaultController : Controller
     {
+        private SubscribeEntities db = new SubscribeEntities();
         // GET: Default
         public void Index()
         {
-            var pushEndpoint = @"https://fcm.googleapis.com/fcm/send/cWm9kR2qJic:APA91bEe0tKuWmmY9HB2zjWD9pDaa3PvwtO7K_vZQSwMjMcdyFArGSJQX-p5Wlg99u6BUlRY4tDPBTAHKdE-vmKb5LUumD6_qVtOLiYQF_ni5pfaTI6rqIpyzuIXry2QwTQ1DpW3H4XC";
-            var p256dh = @"BEAJGx-itpRGenXmiFxNjSujZfnKLOD5vRAH8p5WgTjIw3C5dgcQfH8FErIste4v_j9xeUmd_uCR1jJ4fjjqHy0";
-            var auth = @"ZvmaG95Vt7tggKaJoW6dhA";
-
             //Android bugged
             //var pushEndpoint = @"https://fcm.googleapis.com/fcm/send/cI0P54ekjvU:APA91bESOfyswnaof08mQwMwLoh8CHWiw83VlLuiXw0DDBO6w3wcaPYlw4WUdTTZE2wvD-8CR1d1odQA4zRAVNdlQeb6S_QpQJqwkdE6LlY3UNYRfaGLLqXTIktv4dyzNogghLl480Wc";
             //var p256dh = @"BBqB2J71DRMtswIZMVtAgEBhjWeKmrZD_YGpvDL-3sfx9i4NYsHX4jG6uaosRCopGMrfN3TBO3_myUKwM7nselg";
@@ -26,12 +24,10 @@ namespace FPT_Science.Controllers
             var publicKey = @"BMEf2HHXX9p2gOV9FCn5Ds6sLjYFxEoyctGqh73PfzhUSg_I2MuoyxABXR6EWHLBPdVsup61nAfR1-nEQC2yGYM";
             var privateKey = @"5s_tAAVSEViKHYkE_OjhRvkfoWZYg4s2a3yGGgEC72k";
 
-            var subscription = new PushSubscription(pushEndpoint, p256dh, auth);
             var vapidDetails = new VapidDetails(subject, publicKey, privateKey);
             //var gcmAPIKey = @"[your key here]";
 
-            var message = 
-                new JObject
+            var message = new JObject
             {
                 { "title", "Tin nhắn từ Nguyen" },
                 { "body", "Đang trên lab 1 mình nè" }
@@ -39,14 +35,34 @@ namespace FPT_Science.Controllers
             //new { title = "Yêu cầu hỗ trợ hội nghị", body = "Đã được chấp nhận" };
 
             var webPushClient = new WebPushClient();
-            try
+
+            List<Subscriber> subs = db.Subscribers.ToList();
+
+            List<Subscriber> gone = new List<Subscriber>();
+
+            foreach (Subscriber item in subs)
             {
-                webPushClient.SendNotification(subscription, message.ToString(), vapidDetails);
-                //webPushClient.SendNotification(subscription, "payload", gcmAPIKey);
+                try
+                {
+                    var subscription = new PushSubscription(item.pushEndpoint, item.p256dh, item.auth);
+
+                    webPushClient.SendNotification(subscription, message.ToString(), vapidDetails);
+                    //webPushClient.SendNotification(subscription, "payload", gcmAPIKey);
+                }
+                catch (WebPushException exception)
+                {
+                    if (exception.StatusCode == System.Net.HttpStatusCode.Gone)
+                    {
+                        gone.Add(item);
+                    }
+                    Console.WriteLine("Http STATUS code" + exception.StatusCode);
+                }
             }
-            catch (WebPushException exception)
+
+            if (gone.Count != 0)
             {
-                Console.WriteLine("Http STATUS code" + exception.StatusCode);
+                db.Subscribers.RemoveRange(gone);
+                db.SaveChanges();
             }
         }
     }
--- WebApp/Controllers/SubController.cs
+++ WebApp/Controllers/SubController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using WebApp.Models;
 
 namespace FPT_Science.Controllers
 {
@@ -11,7 +13,27 @@ namespace FPT_Science.Controllers
         // GET: Sub
         public ActionResult Index(string json)
         {
-            return View();
+            SubscribeEntities db = new SubscribeEntities();
+            JObject _json = JObject.Parse(json);
+
+            string endpoint = _json.Value<string>("endpoint");
+            if (db.Subscribers.Find(endpoint) == null)
+            {
+                JObject keys = _json.Value<JObject>("keys");
+                string p256dh = keys.Value<string>("p256dh");
+                string auth = keys.Value<string>("auth");
+
+                Subscriber sub = new Subscriber
+                {
+                    auth = auth,
+                    p256dh = p256dh,
+                    pushEndpoint = endpoint
+                };
+
+                db.Subscribers.Add(sub);
+                db.SaveChanges();
+            }
+            return Redirect("Home");
         }
     }
 }
